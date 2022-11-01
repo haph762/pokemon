c55ba1dd0f96a9e08fd8933a20a385728945d60d
Haph762 <phanha762@gmail.com>
2022-11-01 23:17:37 +0700

    installing entity

--- PokemonReview/Data/DataContext.cs
+++ PokemonReview/Data/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿#nullable disable
+using Microsoft.EntityFrameworkCore;
 using PokemonReview.Models;
 
 namespace PokemonReview.Data
--- PokemonReview/Models/Reviewer.cs
+++ PokemonReview/Models/Reviewer.cs
@@ -4,7 +4,7 @@ namespace PokemonReview.Models
     public class Reviewer
     {
         public int Id { get; set; }
-        public string FistName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public ICollection<Review> Reviews { get; set; }
     }
